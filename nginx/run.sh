e583fc7dab1879e4136aa9a6b005b9581e9878a6
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-12-02 15:42:49 -0800

    update run.sh

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,12 +4,14 @@ set -eu
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+sudo mkdir -p /var/log/nginx
+
 sudo docker run \
     -d \
     --rm \
     -v ${DIR}/nginx.conf:/etc/nginx/nginx.conf:ro \
     -v /var/log:/var/log \
-    -v ${HOME}/.minikube:/etc/certs \
+    -v /home/eprokop/.minikube:/etc/certs \
     --net host \
     --name nginx \
     nginx
